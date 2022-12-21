a1eb198881d242ffad146b77db4f84f23a8fadbf
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-21 08:53:50 +0700

    changes

--- Math_10/src/common/params.py
+++ Math_10/src/common/params.py
@@ -1,14 +1,13 @@
 from math import *
 
-M = 100
+M = 400
 A = 6
-α = 0.02
+α = 0.5
 r = 0.5
 π = 3.1415926535897932384626433832795
-hInitial = 0.1
-x_0 = 0
-controlTime = 30
-order = 4
+hInitial = 0.2
+x_0 = -1
+controlTime = 2
 
 def g(x):
     return sin((π * (x - 1)) / 3)
@@ -20,12 +19,13 @@ def f(x):
 def initU_0(N, h):
     u_0 = [0] * (N + 1)
     for j in range(0, N + 1):
-        if 4 > j * h >= 1:
-            u_0[j] = g(j * h)
-        # if 1 > j * h + x_0 > 0:
-        #     u_0[j] = 2
-        # if 0 >= j * h + x_0 > -1:
-        #     u_0[j] = 3
+        # if 4 > j * h >= 1:
+        #     u_0[j] = g(j * h)
+
+        if 0 >= j * h + x_0:
+            u_0[j] = 2
+        else:
+            u_0[j] = 1
 
     return u_0
 
@@ -36,6 +36,7 @@ def initD(j, n, u, N):
     if j == N:
         return 0.25 * r * (u[n - 1][j - 1]) + u[n - 1][j]
     return 0.25 * r * (u[n - 1][j - 1] - u[n - 1][j + 1]) + u[n - 1][j]
+
 
 def correctTao(N, n, u, h):
     max = 0
@@ -48,7 +49,7 @@ def correctTao(N, n, u, h):
 def rungeCheck(approximator, u1, u2, compareParams):
     maxOrder = 0
     for i in range(approximator.N + 1):
-        localOrder = abs((u1[i] - u2[2 * i]) / (pow(2, order) - 1))
+        localOrder = abs((u1[i] - u2[2 * i]) / (pow(2, approximator.order) - 1))
         if localOrder > maxOrder:
             maxOrder = localOrder
-    print('for k = {0}, {1}, correctness = {2}'.format(order, compareParams, maxOrder))
+    print('for k = {0}, {1}, correctness = {2}'.format(approximator.order, compareParams, maxOrder))
