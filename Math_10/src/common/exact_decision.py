6417cb9076cd66a4bbff27cc2b041aa70f0154c3
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-21 08:53:06 +0700

    changes

--- Math_10/src/common/exact_decision.py
+++ Math_10/src/common/exact_decision.py
@@ -2,23 +2,25 @@ from common.params import *
 
 
 class DecisionInitializer(object):
-    @staticmethod
-    def initY(N, h, τ):
-        y = [[0 for _ in range(N + 1)] for _ in range(M + 1)]
-        for n in range(0, M + 1):
-            for j in range(N + 1):
-                if 4 >= x_0 + j * h - α * τ * n > 1:
-                    y[n][j] = g(j * h + x_0 + α * τ * n)
-        return y
 
     @staticmethod
-    def Y(N, h, T):
+    def Y(N, h, T, num):
         y = [0] * (N + 1)
         for j in range(N + 1):
-            if 4 >= x_0 + j * h - α * T > 1:
-                y[j] = g(j * h - α * T)
+            if num == 1:
+                if 4 >= x_0 + j * h - α * T > 1:
+                    y[j] = g(j * h - α * T)
+            if num == 2:
+                if 0 >= j * h + x_0 - α * T:
+                    y[j] = 2
+                else:
+                    y[j] = 1
+            if num == 3:
+                if 0 >= j * h + x_0:
+                    y[j] = 2
+                else:
+                    y[j] = 1
         return y
-
 
     @staticmethod
     def initX(N, h):
@@ -26,3 +28,4 @@ class DecisionInitializer(object):
         for i in range(N + 1):
             x_grid[i] = i * h + x_0
         return x_grid
+
