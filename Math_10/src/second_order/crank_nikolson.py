932ba3525a5ca025f392a775300d01a459bc9100
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-21 08:55:40 +0700

    changes

--- Math_10/src/second_order/crank_nikolson.py
+++ Math_10/src/second_order/crank_nikolson.py
@@ -6,12 +6,16 @@ class Approximator(object):
         self.h = h
         self.N = N
         self.tao = tao
+        self.order = 2
         self.methodName = 'Crank-Nicholson linear'
         self.u = [[0 for _ in range(N + 1)] for _ in range(M + 1)]
         self.count()
 
     def count(self):
         self.u[0] = initU_0(self.N, self.h)
+        for j in range(1, M + 1):
+            self.u[j][0] = self.u[0][0]
+
         for n in range(1, M + 1):
             self.tridiagonalMatrixRunning(n)
 
@@ -33,5 +37,5 @@ class Approximator(object):
                 nu[j] -= (-0.25 * r * nu[j - 1] - d[j]) / (1 - 0.25 * r * ksi[j - 1])
 
         self.u[n][self.N] = nu[self.N]
-        for j in range(self.N, 0, -1):
+        for j in range(self.N, 1, -1):
             self.u[n][j - 1] = (ksi[j - 1] * self.u[n][j] + nu[j - 1])
