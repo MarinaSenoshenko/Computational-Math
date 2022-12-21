3c01c64fc431b36c493a87a4307fc9742dd824d7
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-21 08:54:22 +0700

    changes

--- Math_10/src/first_order/godunov_scheme.py
+++ Math_10/src/first_order/godunov_scheme.py
@@ -5,12 +5,16 @@ class Approximator(object):
         self.h = h
         self.N = N
         self.tao = tao
+        self.order = 1
         self.methodName = 'Godunov-scheme linear'
         self.u = [[0 for _ in range(self.N + 1)] for _ in range(M + 1)]
         self.count()
 
     def count(self):
         self.u[0] = initU_0(self.N, self.h)
+        for j in range(1, M + 1):
+            self.u[j][0] = self.u[0][0]
+
         for n in range(1, M + 1):
-            for j in range(self.N + 1):
+            for j in range(1, self.N + 1):
                 self.u[n][j] = r * self.u[n - 1][j - 1] + (1 - r) * self.u[n - 1][j]
