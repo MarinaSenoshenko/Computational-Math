b699d9e75b2d6fffea2335ac490b31a2c08d7175
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-24 01:23:30 +0700

    last modify

--- Math_10/src/common/exact_decision.py
+++ Math_10/src/common/exact_decision.py
@@ -4,19 +4,14 @@ from common.params import *
 class DecisionInitializer(object):
 
     @staticmethod
-    def Y(N, h, T, num):
+    def Y(N, h, num):
         y = [0] * (N + 1)
         for j in range(N + 1):
             if num == 1:
-                if 4 >= x_0 + j * h - α * T > 1:
-                    y[j] = g(j * h - α * T)
+                if 4 >= x_0 + j * h - α * controlTime > 1:
+                    y[j] = g(j * h - α * controlTime)
             if num == 2:
-                if 0 >= j * h + x_0 - α * T:
-                    y[j] = 2
-                else:
-                    y[j] = 1
-            if num == 3:
-                if 0 >= j * h + x_0:
+                if 0 >= x_0 + j * h - α * controlTime:
                     y[j] = 2
                 else:
                     y[j] = 1
@@ -28,4 +23,5 @@ class DecisionInitializer(object):
         for i in range(N + 1):
             x_grid[i] = i * h + x_0
         return x_grid
+
 
