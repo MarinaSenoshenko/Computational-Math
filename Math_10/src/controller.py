a8fb1e6b9a9ef4e125073bcd447b8efdc9c35365
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-24 01:27:53 +0700

    last modify

--- Math_10/src/controller.py
+++ Math_10/src/controller.py
@@ -2,13 +2,12 @@ from common.params import *
 from visualize.view import Visualizer
 # from first_order.godunov_scheme import Approximator
 # from second_order.crank_nikolson import Approximator
-from first_order.godunov_scheme_hard import Approximator
-# from second_order.crank_nikolson_hard import Approximator
+# from first_order.godunov_scheme_hard import Approximator
+from second_order.crank_nikolson_hard import Approximator
 
 
 class Controller(object):
     def __init__(self, kff):
-        α = kff
         for i in range(4):
             print("r = {0}, tao = {1}, h = {2}".format(r, (r / kff) * hInitial / pow(2, i), hInitial / pow(2, i)))
 
@@ -17,6 +16,10 @@ class Controller(object):
                    Approximator(hInitial / 4, int((A + abs(x_0)) / (hInitial / 4)), (r / kff) * (hInitial / 4)),
                    Approximator(hInitial / 8, int((A + abs(x_0)) / (hInitial / 8)),
                                 (r / kff) * (hInitial / 8))).showGraphic()
+
+
+
+
 
 
 
