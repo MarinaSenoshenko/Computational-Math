305297fbbc5ea3eef95e3cd5c6900d0015dd9b8e
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-24 01:25:32 +0700

    last modify

--- Math_10/src/first_order/godunov_scheme_hard.py
+++ Math_10/src/first_order/godunov_scheme_hard.py
@@ -2,6 +2,8 @@ from common.params import *
 
 class Approximator(object):
     def __init__(self, h, N, tao):
+        if r > 1:
+            raise ValueError('Incorrect r')
         self.h = h
         self.N = N
         self.tao = tao
@@ -16,8 +18,10 @@ class Approximator(object):
         for j in range(1, M + 1):
             self.u[j][0] = self.u[0][0]
 
-        for n in range(1, M + 1):
-            for j in range(1, self.N + 1):
-                self.tao_cur = correctTao(self.N, n, self.u, self.h)
-                self.u[n][j] = self.u[n - 1][j] - (self.tao_cur / self.h)  * (f(self.u[n - 1][j]) - f(self.u[n - 1][j - 1]))
+        for j in range(1, M + 1):
+            self.u[j][self.N] = self.u[0][self.N]
 
+        for n in range(1, M + 1):
+            self.tao_cur = correctTao(self.N, n, self.u, self.h)
+            for j in range(1, self.N):
+                self.u[n][j] = self.u[n - 1][j] - (self.tao_cur / self.h) * (f(self.u[n - 1][j]) - f(self.u[n - 1][j - 1]))
