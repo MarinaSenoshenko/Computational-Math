ff5dee9146a0cf05695e52c18775d6e005d35398
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-12-24 01:26:55 +0700

    last modify

--- Math_10/src/second_order/crank_nikolson_hard.py
+++ Math_10/src/second_order/crank_nikolson_hard.py
@@ -1,4 +1,5 @@
 from common.params import *
+from order_set import setOrder
 
 
 class Approximator(object):
@@ -12,20 +13,21 @@ class Approximator(object):
         self.u = [[0 for _ in range(N + 1)] for _ in range(M + 1)]
         self.u_prev = [0] * (N + 1)
         self.u_cur = [0] * (N + 1)
+
         self.count()
 
     def count(self):
         self.u[0] = initU_0(self.N, self.h)
-        for j in range(1, M + 1):
-            self.u[j][0] = self.u[0][0]
+        setOrder(self)
 
         for n in range(1, M + 1):
             self.tao_cur = correctTao(self.N, n, self.u, self.h)
             self.tridiagonalMatrixRunning(n)
 
+
     def condition(self):
         for i in range(self.N + 1):
-            if abs(self.u_cur[i] - self.u_prev[i]) > 0.0001:
+            if abs(self.u_cur[i] - self.u_prev[i]) > 0.01:
                 return 1
         return 0
 
@@ -35,18 +37,21 @@ class Approximator(object):
         d = [0] * (self.N + 1)
 
         for j in range(self.N + 1):
-            self.u_prev[j] = 1
-            self.u_cur[j] = 0
+            self.u_prev[j] = 0
+            self.u_cur[j] = 1
 
         while self.condition():
+            for j in range(self.N + 1):
+                self.u_prev[j] = self.u_cur[j]
+
             ksi = [0] * (self.N + 1)
             nu = [0] * (self.N + 1)
             for j in range(self.N + 1):
                 if j != 0:
-                    a[j] = -0.25 * self.u_prev[j - 1] * (self.tao_cur / self.h)
-                d[j] = self.u[n - 1][j]
+                    a[j] = -(self.u_prev[j - 1]) * (self.tao_cur / (4 * self.h))
                 if j != self.N:
-                    c[j] = 0.25 * self.u_prev[j + 1] * (self.tao_cur / self.h)
+                    c[j] = self.u_prev[j + 1] * (self.tao_cur / (4 * self.h))
+                d[j] = self.u[n - 1][j]
 
             for j in range(self.N + 1):
                 if j == 0:
@@ -57,13 +62,12 @@ class Approximator(object):
                         ksi[j] -= c[j] / (1 + a[j] * ksi[j - 1])
                     nu[j] -= (a[j] * nu[j - 1] - d[j]) / (1 + a[j] * ksi[j - 1])
 
-            for j in range(self.N + 1):
-                self.u_prev[j] = self.u_cur[j]
-
             self.u_cur[self.N] = nu[self.N]
             for j in range(self.N, 0, -1):
                 self.u_cur[j - 1] = (ksi[j - 1] * self.u_cur[j] + nu[j - 1])
 
-        for j in range(1, self.N + 1):
+
+        for j in range(2, self.N - 2):
             self.u[n][j] = self.u_cur[j]
+
 
